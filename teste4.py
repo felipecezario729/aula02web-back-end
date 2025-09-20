c308e68c67ef7f5c94ed09ef23f2d498cbb85fb8
FELIPE LOPES CEZARIO <felipe.cezario@aluno.senac.rio>
2025-09-19 21:33:18 -0300

    cadastro de funcionarios

--- teste4.py
+++ teste4.py
@@ -1,4 +1,4 @@
-funcionariosParaCadastrar = [​
+funcionariosParaCadastrar = [
 
     {"nome": "Pablo", "sobrenome": "Araujo", "idade": 34, "altura": 1.71, "temHabilitacao": True},​
 
@@ -9,3 +9,15 @@ funcionariosParaCadastrar = [​
 ]
 
 cadastrosParaEnviarParaOBanco = []
+class pessoas:
+    def __init__(self, nome, sobrenome, idade, altura, habilitacao):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+        self.altura = altura
+        self.habilitacao =  habilitacao
+
+
+    def informacao(self):
+        print(f"O usuario{self.nome} {self.sobrenome} foi salvo com sucesso.")
+
