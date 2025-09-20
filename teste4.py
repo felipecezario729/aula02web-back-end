b908eb3ed61b60c437d9c05d2436d76a9fd206bf
FELIPE LOPES CEZARIO <felipe.cezario@aluno.senac.rio>
2025-09-19 21:25:01 -0300

    Cadastro de Funcionarios

---
+++ teste4.py
@@ -0,0 +1,11 @@
+funcionariosParaCadastrar = [​
+
+    {"nome": "Pablo", "sobrenome": "Araujo", "idade": 34, "altura": 1.71, "temHabilitacao": True},​
+
+    {"nome": "Ana", "sobrenome": "Silva", "idade": 28, "altura": 1.65, "temHabilitacao": False},​
+
+    {"nome": "Carlos", "sobrenome": "Souza", "idade": 40, "altura": 1.80, "temHabilitacao": True}​
+
+]
+
+cadastrosParaEnviarParaOBanco = []
